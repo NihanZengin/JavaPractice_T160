b9dcbe299c0ad6c548368ee72c17ae191b29fd4c
NihanZengin <nihan.zengin90@gmail.com>
2025-01-17 16:23:03 +0100

    Odev

--- src/ODEV/Odev1.java
+++ src/ODEV/Odev1.java
@@ -14,5 +14,19 @@ public class Odev1 {
             5 – Kart İade
         ********** WISE BANK  **********
          */
+//        System.out.println("********** WISE BANK **********");
+//        System.out.println("   WISE ATM'YE HOŞGELDİNİZ");
+//        System.out.println("   1 - Bakiye Sorgulama");
+//        System.out.println("   2 - Para Yatırma");
+//        System.out.println("   3 - Para Çekme");
+//        System.out.println("   4 - Bilgi Güncelleme");
+//        System.out.println("   5 - Kart İade");
+//        System.out.println("********** WISE BANK **********");
+
+        System.out.println("********** WISE BANK  **********"+"\n    WISE ATM’YE HOŞGELDİNİZ"+
+                "\n    1 – Bakiye Sorgulama"+"\n    2 – Para Yatırma"+"\n    3 – Para Çekme"+
+                "\n    4 – Bilgi Güncelleme"+"\n    5 – Kart İade"+"\n********** WISE BANK  **********");
+
+
     }
 }
--- src/ODEV/Odev2.java
+++ src/ODEV/Odev2.java
@@ -1,5 +1,7 @@
 package ODEV;
 
+import java.util.Scanner;
+
 public class Odev2 {
     public static void main(String[] args) {
 
@@ -15,6 +17,53 @@ public class Odev2 {
         şeklinde sistemimize kaydınız başarıyla tamamlanmıştır.
 
          */
+        Scanner scan= new Scanner(System.in);
+
+        System.out.print("Lutfen Isim giriniz...");
+        String isim=scan.nextLine();
+
+        System.out.print("Lutfen Soyisim giriniz...");
+        String soyisim=scan.nextLine();
+
+        System.out.print("Lutfen Yasinizi giriniz...");
+        int yas=scan.nextInt();
+
+        //  mail adresinizi giriniz der demez sifre giriniz diyor ve mail girmeme izin vermiyor
+        //Lutfen Mail Adresi giriniz...Lutfen Şifre giriniz...
+        // nextInt() metodu, sayısal bir giriş aldığında yeni bir satıra geçmez;
+        // bu yüzden bir sonraki nextLine() çağrısında,
+        // önceki satırdaki yeni satır karakterini (\n) okur ve hemen çalışır.
+        //
+        //Bu durumu çözmek için nextInt()'ten sonra bir scan.nextLine() kullanarak
+        // boş bir satır okuyarak tamponu temizlememiz gerekmektedir.
+
+        scan.nextLine(); // Sorunu cozmek icin
+
+        System.out.print("Lutfen Mail Adresi giriniz...");
+        String mAdres=scan.nextLine();
+
+        System.out.print("Lutfen Şifre giriniz...");
+        String sifre=scan.nextLine();
+
+        System.out.println("******       KAYIT BAŞARILI      ******"+"\n    Adınız: "+isim+
+                "\n    Soyadınız: "+soyisim+"\n    Yaşınız:"+yas+"\n    Mail Adresiniz:"+mAdres+
+                "\n    Şifreniz: "+sifre);
+
+
+/* TERMINAL CIKTI:
+ * Lutfen Isim giriniz...Tarik
+ * Lutfen Soyisim giriniz...Yigit
+ * Lutfen Yasinizi giriniz...44
+ * Lutfen Mail Adresi giriniz...tarik@yigit.com.tr
+ * Lutfen Şifre giriniz...A2e365
+ * ******       KAYIT BAŞARILI      ******
+ *     Adınız: Tarik
+ *     Soyadınız: Yigit
+ *     Yaşınız:44
+ *     Mail Adresiniz:tarik@yigit.com.tr
+ *     Şifreniz: A2e365
+ */
+
 
     }
 
